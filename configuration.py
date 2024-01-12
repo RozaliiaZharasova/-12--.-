8ec9aba9de4a90e314235783b16a7b03b5fe14f6
rozaliazarasova <rozaliazharasova@yandex.ru>
2024-01-12 22:24:27 +0100

    First change

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
 #Розалия Жарасова, 12-я когорта — Финальный проект. Инженер по тестированию плюс
-BASE_URL = ("https://91436bd5-664e-4b4f-8c1e-7aa30dfcf655.serverhub.praktikum-services.ru")
+BASE_URL = ("https://dbb1a524-81c0-4e59-a1d0-d18204abf66a.serverhub.praktikum-services.ru")
 CREAT_ORDERS = "/api/v1/orders"
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -13,17 +13,3 @@ def get_order(track_number):
     get_order_url = f"{configuration.BASE_URL}/api/v1/orders/track?t={track_number}"
     response = requests.get(get_order_url)
     return response
-
-# Автотест по шагам
-def test_order_creation_and_retrieval():
-    response = create_order(data.order_body)
-
-    track_number = response.json()["track"]
-    print("Заказ создан. Номер трека:", track_number)
-
-    order_response = get_order(track_number)
-
-    assert order_response.status_code == 200, f"Ошибка: {order_response.status_code}"
-    order_data = order_response.json()
-    print("Данные заказа:")
-    print(order_data)
