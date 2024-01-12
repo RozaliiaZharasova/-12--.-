aac0a90254305fc1b049f494f5857afe0e19c3e5
rozaliazarasova <rozaliazharasova@yandex.ru>
2024-01-12 16:09:09 +0100

    add file

---
+++ configuration.py
@@ -0,0 +1,3 @@
+#Розалия Жарасова, 12-я когорта — Финальный проект. Инженер по тестированию плюс
+BASE_URL = ("https://91436bd5-664e-4b4f-8c1e-7aa30dfcf655.serverhub.praktikum-services.ru")
+CREAT_ORDERS = "/api/v1/orders"
